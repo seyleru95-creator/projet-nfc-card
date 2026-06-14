b73736a79ee21329ee302e046b0e03c5f658b9fb
Lou Siffredi <lousiffredi@Mac.lan>
2026-06-14 21:50:05 +0200

    fix js entry point

--- scripts/build-html.cjs
+++ scripts/build-html.cjs
@@ -1,7 +1,7 @@
 const fs = require('fs');
 const assets = fs.readdirSync('dist/client/assets');
 const css = assets.find(f => f.endsWith('.css'));
-const js = assets.find(f => f.startsWith('index-') && f.endsWith('.js') && !f.includes('11GpYcmB') && !f.includes('BtlMrHY'));
+const js = assets.find(f => f.startsWith('index-') && f.endsWith('.js') && f.includes('BtlMrHY'));
 
 const html = `<!DOCTYPE html>
 <html lang="fr">
